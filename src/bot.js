8e55612ff80dff2207ef29a5024553bdce340423
ambernick <ambernich2@yahoo.com>
2021-02-17 20:29:53 -0500

    add basic event listeners between bot and discord

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,16 @@
 require('dotenv').config(); //loads env variables
 
+//create Client class to use discord library
 const { Client } = require('discord.js');
 const client = new Client();
+
+//create events that happen through discord
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`)
+});
+
+client.on('message', (message) => {
+    console.log(`[${message.author.tag}]: ${message.content}`);
+})
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
