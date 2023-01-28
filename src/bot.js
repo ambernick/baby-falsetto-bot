9a691f60a8417be1e150422952b81f8b7a3c17c2
ambernick <ambernich2@yahoo.com>
2023-01-27 23:21:10 -0500

    added ping command, updated node and discord.js

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,10 @@
 require('dotenv').config(); //loads env variables
 
-//create Client class to use discord library
-const { Client } = require('discord.js');
-const client = new Client();
+const fs = require('node:fs');
+const path = require('node:path');
+//create Client class to use discord library/ client instants
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds] });
 
 //create events that happen through discord
 client.on('ready', () => {
@@ -13,16 +15,25 @@ client.on('message', (message) => {
     console.log(`[${message.author.tag}]: ${message.content}`);
 })
 
+client.on(Events.InteractionCreate, interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
+//login with client token
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
-const opts = {
-    identity: {
-      username: '<babyfalsettobot>',
-      password: '<>'
-    },
-    channels: [
-      '<falsettovibrato>'
-    ]
-  };
+client.commands = new Collection();
 
-  
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
